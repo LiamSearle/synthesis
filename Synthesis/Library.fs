762cdd778c7ddb98fe0de6c8ff1551cef275be82
Liam Searle <liamsearle7@gmail.com>
2019-03-10 20:34:08 +0200

    working on it

--- Synthesis/Library.fs
+++ Synthesis/Library.fs
@@ -54,24 +54,110 @@ let digits x =
 
 let minmax (a,b,c,d) =
   let x = min (min a b) (min c d) 
-  let y = max (max a b) (min c d) 
+  let y = max (max a b) (max c d) 
   (x,y)
     //failwith "Not implemented"
 
-let isLeap _ =
-    failwith "Not implemented"
+let isLeap x =
+  match x<1582 with  
+    |true -> failwith "Year out of bounds"
+    |_ -> match x%100=0 with
+            |true -> x%4 = 0 && x%400 = 0
+            |_ -> x%4 = 0
+    
+    
+    (*match ((x%4 = 0) || ((x%100 = 0) && (x%400 = 0))) with
+            |true -> true
+            |_ -> false*)
 
-let month _ =
-    failwith "Not implemented"
+    //failwith "Not implemented"
 
-let toBinary _ =
-    failwith "Not implemented"
+let month = function
+    |1 -> "January",31
+    |2 -> "February",28
+    |3 -> "March",31
+    |4 -> "April",30
+    |5 -> "May",31
+    |6 -> "June",30
+    |7 -> "July",31
+    |8 -> "August",31
+    |9 -> "September",30
+    |10 -> "October",31
+    |11 -> "November",30
+    |12 -> "December",31
+    |_ -> failwith "Invalid Month"
+    //failwith "Not implemented"
 
-let bizFuzz _ =
-    failwith "Not implemented"
+let rec toBinary i =
+  match i<0 with
+  |true -> failwith "Negative Number"
+  |_ -> match i with
+        | 0 | 1 -> string i
+        | _ ->
+          let bit = string (i % 2)
+          (toBinary (i / 2)) + bit
+  //failwith "Not implemented"
 
-let monthDay _ _ =
-    failwith "Not implemented"
+let bizFuzz x = 
+  match x > 0 with
+  | false -> (0,0,0)
+  |_ -> ((x/3),(x/5),((x/3)/5))
+    //failwith "Not implemented"
+
+(*.
+Create a function ​monthDay​ which accepts an integer ​d​ and a year ​y​, and returns astring for the month that the day ​d​ 
+falls within.  The function must accept a range ofd​ from 1 to 365 if ​y​ isn’t a leap year, and must accept ​d​ between 1 and
+366 if ​y​ is aleap year.  If ​d​ is out of range, or if ​y​ is less than 1582, then an exception must bethrown.  
+Remember that:
+ a.April, June, September, and November have 30 days.
+ b.January, March, May, July, August, October, and December have 31 days.
+ c.February has 29 days in a leap year, and 28 days otherwise.)
+*)
+
+let monthDay day year =
+  match isLeap(year) with
+    |true -> match day<367 with 
+      |false -> failwith "day out of range"
+      |_-> 
+        match day<32 with
+          | true -> "January"
+        match day<61 && day > 32 with
+          | true -> "February"
+        match day<91 && day> 60 with
+          |true -> "March"
+        match day<121 && day> 91 with
+          |true -> "April"
+        match day<152 && day> 121 with
+          |true -> "May"
+        match day<182 && day> 152 with
+          |true -> "June"
+        match day<213 && day> 182 with
+          |true -> "July"
+        match day<242 && day> 213 with
+          |true -> "August"
+        match day<272 && day> 242 with
+          |true -> "September"
+        match day<303 && day> 272 with
+          |true -> "October"
+        match day<91 && day> 60 with
+          |true -> "November"
+        
+        
+        
+        
+        
+        
+        
+
+        //todo 
+    |_-> match day<366 with
+      |false -> failwith "day out of range"
+      |_->let month = function
+            | <=31 -> "January"//todo
+            | _ -> failwith "idk what happened"
+          
+            
+  //failwith "Not implemented"
 
 let coord _ =
     failwith "Not implemented"
